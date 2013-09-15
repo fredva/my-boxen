96e65bdab258e9a546fb9b91faf55ae0d48822f7
Fredrik Valdmanis <fredrik.valdmanis@bekk.no>
2013-09-15 11:44:02 +0200

    Added vim to personal manifest

--- modules/people/manifests/fredva.pp
+++ modules/people/manifests/fredva.pp
@@ -1,7 +1,39 @@
 class people::fredva {
   
+  $home = "/Users/${::boxen_user}"
+  
   include "alfred"
   include "macvim"
   include "spotify"
 
+  package {
+    'vim': ; 
+  }
+
+  # Set up vim
+  $vimrc = "${home}/.vimrc" 
+  $vimdir = "${home}/.vim" 
+  $dotvim_repo = "${boxen::config::srcdir}/dotvim"
+
+  repository { $dotvim_repo:
+    source  => 'fredva/dotvim',
+    notify => Exec["update_dotvim_git_submodules"]
+  }
+
+  exec { "update_dotvim_git_submodules":
+    command => "cd $dotvim_repo && git submodule init && git submodule update",
+    provider => "shell",
+    refreshonly => true,
+    user => $::boxen_user
+  }
+
+  file { $vimrc:
+    target => "${dotvim_repo}/vimrc",
+    require => Repository[$dotvim_repo] 
+  }
+
+  file { $vimdir: 
+    target => $dotvim_repo,
+    require => Repository[$dotvim_repo] 
+  }
 }
