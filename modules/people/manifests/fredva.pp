c68e2982c74e0e354edc10cc5e340f8fd6cbf8e8
Fredrik Valdmanis <fredrik.valdmanis@bekk.no>
2013-09-15 14:27:37 +0200

    Added exec for generating boxen config for fish.

--- modules/people/manifests/fredva.pp
+++ modules/people/manifests/fredva.pp
@@ -43,13 +43,24 @@ class people::fredva {
 
   # Set up dotfiles
   $dotfiles_repo = "${boxen::config::srcdir}/dotfiles"
+  $fish_config = "${home}/.config/fish"
   repository { $dotfiles_repo:
-    source => 'fredva/dotfiles'
+    source => 'fredva/dotfiles',
+    notify => Exec["generate boxen config for fish"]
   }
 
-  file { "${home}/.config/fish":
+  exec { "generate boxen config for fish":
+    command => "$home/.config/fish/boxen-to-fish.rb",
+    provider => 'shell',
+    refreshonly => true,
+    user => $::boxen_user
+  }
+
+  file { $fish_config: 
     ensure => 'link',
     target => "${dotfiles_repo}/fish",
     require => Repository[$dotfiles_repo]
   }
+
+
 }
